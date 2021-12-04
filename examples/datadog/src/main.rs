71c83ee8d5159eb464f23985034376e605bef30a
Zhongyang Wu <zhongyang.wu@outlook.com>
2021-12-04 16:43:28 -0500

    feat: try to reproduce open-telemetry/opentelemetry-rust#674

--- examples/datadog/src/main.rs
+++ examples/datadog/src/main.rs
@@ -1,39 +1,54 @@
-use opentelemetry::global;
-use opentelemetry::global::shutdown_tracer_provider;
-use opentelemetry::{
-    trace::{Span, TraceContextExt, Tracer},
-    Key,
-};
-use opentelemetry_datadog::{new_pipeline, ApiVersion};
 use std::thread;
 use std::time::Duration;
 
-fn bar() {
-    let tracer = global::tracer("component-bar");
-    let mut span = tracer.start("bar");
-    span.set_attribute(Key::new("span.type").string("sql"));
-    span.set_attribute(Key::new("sql.query").string("SELECT * FROM table"));
-    thread::sleep(Duration::from_millis(6));
-    span.end()
-}
+use bytes::Bytes;
+use http::{Request, Response};
+use opentelemetry::{
+    Key,
+    trace::{Span, TraceContextExt, Tracer},
+};
+use opentelemetry::global;
+use opentelemetry::global::shutdown_tracer_provider;
+use opentelemetry::runtime::Tokio;
+use opentelemetry::sdk::trace;
+use opentelemetry::sdk::trace::Sampler;
+use opentelemetry_datadog::{ApiVersion, new_pipeline};
+use opentelemetry_http::{HttpClient, HttpError};
+use tracing::span;
+use tracing_opentelemetry::OpenTelemetryLayer;
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::util::SubscriberInitExt;
 
-fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync + 'static>> {
+// Start a datadog agent using docker
+// docker run --rm -d\
+//     -v /var/run/docker.sock:/var/run/docker.sock:ro \
+//     -v /proc/:/host/proc/:ro \
+//     -v /sys/fs/cgroup/:/host/sys/fs/cgroup:ro \
+//     -p 127.0.0.1:8126:8126/tcp \
+//     -e DD_API_KEY="<API_TOKEN>" \
+//     -e DD_APM_ENABLED=true \
+//     datadog/agent:latest
+//
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync + 'static>> {
     let tracer = new_pipeline()
-        .with_service_name("trace-demo")
+        .with_trace_config(trace::config().with_sampler(Sampler::AlwaysOn))
+        .with_service_name("test")
         .with_version(ApiVersion::Version05)
-        .install_simple()?;
+        .with_agent_endpoint("http://localhost:8126")
+        .install_batch(Tokio)
+        .expect("failed to initialize tracing pipeline");
 
-    tracer.in_span("foo", |cx| {
-        let span = cx.span();
-        span.set_attribute(Key::new("span.type").string("web"));
-        span.set_attribute(Key::new("http.url").string("http://localhost:8080/foo"));
-        span.set_attribute(Key::new("http.method").string("GET"));
-        span.set_attribute(Key::new("http.status_code").i64(200));
+    tracing_subscriber::fmt()
+        .with_ansi(false)
+        .with_writer(std::io::stderr)
+        .with_target(false)
+        .finish()
+        .with(OpenTelemetryLayer::new(tracer))
+        .init();
 
-        thread::sleep(Duration::from_millis(6));
-        bar();
-        thread::sleep(Duration::from_millis(6));
-    });
+    span!(tracing::Level::INFO, "expensive_step_1");
 
     shutdown_tracer_provider();
 
